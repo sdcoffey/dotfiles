d35716f9d9b9fc848f2315390659b90dd50faeab
sdcoffey <stevendcoffey@gmail.com>
2026-03-11 09:20:50 -0700

    fix islist

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,10 @@
 vim.g.mapleader = "\\"
 vim.g.maplocalleader = "\\"
 
+if vim.islist == nil then
+  vim.islist = vim.tbl_islist
+end
+
 require("config.options")
 require("config.keymaps")
 require("config.lazy")
